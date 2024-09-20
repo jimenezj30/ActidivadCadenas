9459c522a7962028f0bde23be33179cc92773ae4
Luis Fernando Jimenez <luisfj3012@gmail.com>
2024-09-20 17:54:27 -0500

    Se agregan los cambios de actividad2cadenas

---
+++ actividad2cadenas/CadenasCaracteres.py
@@ -0,0 +1,42 @@
+#1
+name = "luis"
+age = 19
+
+favouriteFood = "carne y pollo"
+text = f"Hola! Mi nombre es {name}. Yo tengo {age} años, y mi comida favorita es {favouriteFood}"
+print(text)
+
+#2
+FullName = "Luis Jimenez"
+lengthUsuario = len(FullName.strip())
+print(f"Hola, {FullName}! Tu nombre tienen {lengthUsuario} letras, excluyendo los espacios")
+
+#3
+increaseSalesPercent = 12.93720081
+revenueGrowthPercent = 18.33206078
+print( f"Las ventas de la empresa lactea aumentaron un {round(increaseSalesPercent,2)}% y los ingresos crecieron un {round(revenueGrowthPercent,2)}%")
+
+#4
+#secretMessage = "aS!Ir waQm VL!eDafrcnXin=gS .P,yytahgoln.!"
+
+
+#5
+text = "El nombre 'Python' viene dado por la aficion de Van Rossum al grupo Monty Python"
+palabra = text.split()
+numero_palabras = len(palabra)
+print(f"Numero de palabras de la frase: {numero_palabras}")
+
+#6
+word = "camila"
+cambioletra =word.replace('a','e')
+print(cambioletra)
+
+#7
+sentence = "la historia del lenguaje de programación Python"
+palabras = sentence.split()
+inversa = palabras[::-1]
+sentence_invertir = " ".join(inversa)
+print(sentence_invertir)
+
+
+
